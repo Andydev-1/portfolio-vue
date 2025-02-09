ccc32f6781be3f66381e9c686bf68f08e9522544
Andydev-1 <andsuptony@gmail.com>
2025-02-09 11:02:19 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<template>
+  <header>
+    <button @click="count++" class="bg-blue-500 text-white p-2 rounded-md">Click me</button>
+    <p>Count is {{ count }}</p>
+  </header>
+</template>
