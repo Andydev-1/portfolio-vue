72d086b9019e0669ecb9d3c87212c7db7a71f315
Andydev-1 <andsuptony@gmail.com>
2025-02-09 11:18:40 +0100

    added hello world

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ const count = ref(0);
 
 <template>
   <header>
+    <h1>Hello World</h1>
     <button @click="count++" class="bg-blue-500 text-white p-2 rounded-md">Click me</button>
     <p>Count is {{ count }}</p>
   </header>
