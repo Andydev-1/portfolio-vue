17a99d32528f0732d028f9e4ea26191bac3768b8
Andydev-1 <andsuptony@gmail.com>
2025-08-08 16:44:39 +0100

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
-    darkMode: 'class',
-    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                bricolage: ['Bricolage Grotesque', 'sans-serif'],
-            },
-=======
-    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-    theme: {
-        extend: {
-            animation: {
-                'ping-slow': 'ping 3s cubic-bezier(0, 0, 0.2, 1) infinite',
-            },
-            colors: {
-                'primary': '#334155', // Soft Navy
-                'secondary': '#E5E7EB', // Light Gray
-                'accent': '#14B8A6', // Teal Green
-            }
->>>>>>> f73cf9e4394cdd5da92d32a8ab48aa0eee2bd59c
-        },
+  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bricolage: ['Bricolage Grotesque', 'sans-serif'],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
--- vite.config.js
+++ vite.config.js
@@ -15,11 +15,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-<<<<<<< HEAD
   },
   define: {
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
-=======
->>>>>>> f73cf9e4394cdd5da92d32a8ab48aa0eee2bd59c
+
   }
 })
